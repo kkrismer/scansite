21c4918aec076e4fd3cb47931e5e0d2b061c9510
Thomas Bernwinkler <tbernwin@mit.edu>
2017-01-30 14:07:54 -0500

    * Database Search Phosphosite references now shows the inquired information

--- Scansite4/src/main/java/edu/mit/scansite/server/dispatch/handler/features/DatabaseScanHandler.java
+++ Scansite4/src/main/java/edu/mit/scansite/server/dispatch/handler/features/DatabaseScanHandler.java
@@ -1,27 +1,30 @@
 package edu.mit.scansite.server.dispatch.handler.features;
 
-import javax.servlet.ServletContext;
-
-import net.customware.gwt.dispatch.server.ActionHandler;
-import net.customware.gwt.dispatch.server.ExecutionContext;
-import net.customware.gwt.dispatch.shared.ActionException;
-import net.customware.gwt.dispatch.shared.DispatchException;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.google.inject.Inject;
 import com.google.inject.Provider;
-
+import edu.mit.scansite.server.ServiceLocator;
+import edu.mit.scansite.server.dataaccess.DaoFactory;
+import edu.mit.scansite.server.dataaccess.SiteEvidenceDao;
 import edu.mit.scansite.server.dispatch.BootstrapListener;
 import edu.mit.scansite.server.dispatch.handler.user.LoginHandler;
 import edu.mit.scansite.server.features.DatabaseScanFeature;
 import edu.mit.scansite.shared.dispatch.features.DatabaseScanAction;
 import edu.mit.scansite.shared.dispatch.features.DatabaseScanResult;
+import edu.mit.scansite.shared.transferobjects.DatabaseSearchScanResultSite;
+import net.customware.gwt.dispatch.server.ActionHandler;
+import net.customware.gwt.dispatch.server.ExecutionContext;
+import net.customware.gwt.dispatch.shared.ActionException;
+import net.customware.gwt.dispatch.shared.DispatchException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletContext;
+import java.util.Set;
 
 /**
  * @author Tobieh
  * @author Konstantin Krismer
+ * @author Thomas Bernwinkler
  */
 public class DatabaseScanHandler implements
 		ActionHandler<DatabaseScanAction, DatabaseScanResult> {
@@ -47,10 +50,30 @@ public class DatabaseScanHandler implements
 		try {
 			DatabaseScanFeature feature = new DatabaseScanFeature(
 					BootstrapListener.getDbConnector(contextProvider.get()));
-			return feature.doDatabaseSearch(action.getMotifSelection(), action
+			DatabaseScanResult result = feature.doDatabaseSearch(action.getMotifSelection(), action
 					.getDataSource(), action.getRestrictionProperties(), action
 					.getOutputListSize(), true, !loginHandler
 					.isSessionValidLogin(action.getUserSessionId()), contextProvider.get().getRealPath("/"));
+
+            DaoFactory factory = ServiceLocator.getInstance().getDaoFactory(
+                    BootstrapListener.getDbConnector(contextProvider.get()));
+            // check if the sites are PhosphoSiteSites if swissprot is selected
+
+            for (DatabaseSearchScanResultSite site : result.getDbSearchSites()) {
+				Set<String> accessions = (site.getProtein().getAnnotation("accession"));
+                if (accessions != null && !accessions.isEmpty()) {
+                    SiteEvidenceDao evidenceDao = factory.getSiteEvidenceDao();
+                    try {
+                        site.getSite().setEvidence(evidenceDao.getSiteEvidence(
+                                accessions, site.getSite().getSite()));
+                    } catch (Exception e) {
+                        logger.error("Error checking PSP database: "
+                                + e.toString());
+                    }
+                }
+            }
+
+			return result;
 		} catch (Exception e) {
 			logger.error("Error running database search: " + e.toString());
 			throw new ActionException(e);
